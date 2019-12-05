0826d4b4b9175d4e0731e08a679aaa027790885c
italianassassin3 <jose.frontanez@upr.edu>
2019-12-04 23:53:33 -0400

    adding code for lexer not tested and starting parser

--- SCL_Lexer.py
+++ SCL_Lexer.py
@@ -6,5 +6,34 @@ import ply.yacc as yacc
 import ply.lex as lex
 from ply.lex import TOKEN
 
-# TODO: Implement Tokens
 
+reserved = {
+    "hostName":"hostName",
+    "port":"port",
+    "openServer(hostName, port)": "openServer(hostName, port)",
+    "openClient(hostName, port)":"openClient(hostName, port)",
+    "closeServers()": "closeServers()"
+
+}
+
+#Create Tokens
+tokens = list(reserved.values())
+
+t_ignore = r' '
+
+def t_method(t):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    if t.value in reserved:
+        t.type = reserved[t.value]
+    else:
+        t.type = 'hostName'
+    return t
+
+
+def t_error(t):
+    print("Unrecognized Character(s)")
+    print(t)
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
--- SCL_Parser.py
+++ SCL_Parser.py
@@ -1,10 +1,18 @@
 # ServerCommLanguage (SCL) Parser
 
 import ply.yacc as yacc
+import
+tokens =
+names = {}
 
-# TODO: Implement parsing rules
+
 
 
 # called from ServerComm
 def get_yacc_parser():
     return yacc.yacc()
+
+def p_error(s):
+    parser.parse(s)
+
+parser = yacc.yacc()
