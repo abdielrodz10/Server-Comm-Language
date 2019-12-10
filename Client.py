bbcdf2ce09e59aed0afab2bc1b49cc304c4ad6b2
SaberCole <christopher.ramos6@upr.edu>
2019-12-10 15:09:54 -0400

    added server methods

--- Client.py
+++ Client.py
@@ -1,2 +1,20 @@
-# Local client class declaration
+import socket
 # TODO: Implement
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'close':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
